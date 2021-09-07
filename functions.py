8ec88900bbab65940090005ea9e8ca73a7842d96
quisst <supercms04@gmail.com>
2021-09-07 15:04:00 +0900

    검색 속도 향상

--- functions.py
+++ functions.py
@@ -4,9 +4,8 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 import math
-
+import time
 
 nickname = input("닉네임을 입력해주세요. : ")
 
@@ -26,7 +25,7 @@ def research():
     driver.find_element_by_id('SummonerRefreshButton').click()
 
     try:
-        WebDriverWait(driver, 10).until(EC.alert_is_present())
+        WebDriverWait(driver, 5).until(EC.alert_is_present())
         alert = Alert(driver)
         alert.accept()
     except:
@@ -39,8 +38,9 @@ def research():
     num = math.ceil(spin / 20)
     for i in range(num-1):
         driver.find_element_by_xpath('//*[@id="SummonerLayoutContent"]/div[2]/div[2]/div/div[2]/div[{0}]/a'.format(i+4)).click()
-        time.sleep(5)
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="SummonerLayoutContent"]/div[2]/div[2]/div/div[2]/div[{0}]/a'.format(i+4))))
 
+    time.sleep(1)
     html_code = driver.page_source
 
     return html_code, spin
@@ -53,7 +53,7 @@ def get_info(html_code):
     game_length = html.select('.GameLength')[0].get_text()
 
     minute, second = game_length.split('분')
-    second, second_a = second.split('초')
+    second= second.split('초')[0]
 
     result = ''.join(result.split())
 
